58e2cab1cf4a80be09203d50963c2fc4d0cd9776
Alejandro Arco <alejandroarpu@gmail.com>
2024-11-16 15:07:40 +0100

    feat: add last feature of resize

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const DEFAULT_SIZE = 16;
+const VALID_INPUT_SIZE = /^([0-9]|[1-7][0-9])$/;
+const CONTAINER_SIZE = 400;
 let container = document.querySelector(".container");
 
 const gridMouseOver = (event) => {
@@ -5,16 +8,39 @@ const gridMouseOver = (event) => {
   event.target.style.filter = "blur(0.5px)";
 };
 
-for (let j = 1; j <= 40 * 40; j++) {
-  let newDiv = document.createElement("div");
-  newDiv.addEventListener("mouseover", gridMouseOver);
-  newDiv.style.width = `${400 / 40}px`;
-  newDiv.style.height = `${400 / 40}px`;
-  newDiv.classList.add("grid");
-  container.appendChild(newDiv);
-  if (j % 40 === 0) {
-    let newDiv2 = document.createElement("div");
-    newDiv2.classList.add("test");
-    container.appendChild(newDiv2);
+const createGrid = (width, height, containerSize) => {
+  for (let j = 1; j <= width * height; j++) {
+    let newDiv = document.createElement("div");
+    newDiv.addEventListener("mouseover", gridMouseOver);
+    newDiv.style.width = `${containerSize / width}px`;
+    newDiv.style.height = `${containerSize / height}px`;
+    newDiv.classList.add("grid");
+    container.appendChild(newDiv);
+    if (j % width === 0) {
+      let newDiv2 = document.createElement("div");
+      newDiv2.classList.add("test");
+      container.appendChild(newDiv2);
+    }
   }
-}
+};
+
+const resizeGrid = () => {
+  let size = prompt("Please enter the size of grid");
+  let allGrids = document.querySelectorAll(".grid");
+  allGrids.forEach((element) => {
+    element.remove();
+  });
+  if (VALID_INPUT_SIZE.test(size) && size) {
+    createGrid(size, size, CONTAINER_SIZE);
+  } else {
+    alert(
+      "You did not enter a valid number or less than 80! Applied default 16x16."
+    );
+    createGrid(DEFAULT_SIZE, DEFAULT_SIZE, CONTAINER_SIZE);
+  }
+};
+
+createGrid(DEFAULT_SIZE, DEFAULT_SIZE, CONTAINER_SIZE);
+
+let button = document.querySelector("button");
+button.addEventListener("click", resizeGrid);
