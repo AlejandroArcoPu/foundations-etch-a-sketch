7866672186f8cf72ce37e25644509877ff53184b
Alejandro Arco <alejandroarpu@gmail.com>
2024-11-16 14:27:47 +0100

    feat: add some style in button and add cursor as pen

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const gridMouseOver = (event) => {
   event.target.style.filter = "blur(0.5px)";
 };
 
-for (let j = 1; j <= 16 * 16; j++) {
+for (let j = 1; j <= 40 * 40; j++) {
   let newDiv = document.createElement("div");
   newDiv.addEventListener("mouseover", gridMouseOver);
-  newDiv.style.width = `${400 / 16}px`;
-  newDiv.style.height = `${400 / 16}px`;
+  newDiv.style.width = `${400 / 40}px`;
+  newDiv.style.height = `${400 / 40}px`;
   newDiv.classList.add("grid");
   container.appendChild(newDiv);
-  if (j % 16 === 0) {
+  if (j % 40 === 0) {
     let newDiv2 = document.createElement("div");
     newDiv2.classList.add("test");
     container.appendChild(newDiv2);
